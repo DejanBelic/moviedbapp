c303d51e203baf222e164f0bdd65c4bc096763d2
Dejan <dejan.belic@develomon.com>
2018-11-06 11:10:21 +0100

    Initial commit.

---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './MovieThumb.css';
+import { Link } from 'react-router-dom';
+
+const MovieThumb = (props) => {
+  return (
+    <div className='rmdb-moviethumb'>
+        {     props.clickable ? 
+            <Link to={{pathname: `/${props.movieId}`, movieName: `${props.movieName}`}}>
+              <img src={props.image} alt='movieThumb' />
+            </Link>
+              : 
+              <img src={props.image} alt='movieThumb' />
+        }
+      
+    </div>
+  )
+}
+
+export default MovieThumb;
