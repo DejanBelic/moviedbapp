58a6aa240fb7c79fa823955cfa5af84fa6649b99
Dejan <dejan.belic@develomon.com>
2018-11-06 11:21:54 +0100

    changed to hashrouter insted browserrouter.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import NotFound from '../NotFound/NotFound';
 import Movie from '../Movie/Movie';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <React.Fragment>
                 <Header />
                 <Switch>
@@ -16,7 +16,7 @@ const App = () => {
                   <Route component={NotFound} />
                 </Switch>
             </React.Fragment>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
